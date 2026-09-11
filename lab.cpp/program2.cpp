21d43324524c9a3b83f816912dc7ef0db674414c
dynamicgaming740-hash <dynamicgaming740@gmail.com>
2026-09-11 09:52:06 +0530

    programs

--- lab.cpp/program2.cpp
+++ lab.cpp/program2.cpp
@@ -3,10 +3,63 @@
  #include<iostream>
  
  using namespace std;
- class 
+ class student{
+    private:
+            string name;
+            int roll;
+            string branch;
+            float marks;
+    public:
+        void input(string n, int r, string b, float m){
+            name = n;
+            roll = r;
+            branch = b;
+            marks = m;
+        }
+        void show(){
+            cout << "Name: " << name << endl;
+            cout << "Roll: " << roll << endl;
+            cout << "Branch: " << branch << endl;
+            cout << "Marks: " << marks << endl;
+        }
+        
+ };
  
  
  int main(){
+    student s1, s2;
+    string name;
+    int roll;
+    string branch;
+    float marks;
+ 
+    cout << "Enter details for student 1:" << endl;
+    cout << "Name: ";
+    cin >> name;
+    cout << "Roll: ";
+    cin >> roll;
+    cout << "Branch: ";
+    cin >> branch;
+    cout << "Marks: ";
+    cin >> marks;
+    s1.input(name, roll, branch, marks);
+ 
+    cout << "Enter details for student 2:" << endl;
+    cout << "Name: ";
+    cin >> name;
+    cout << "Roll: ";
+    cin >> roll;
+    cout << "Branch: ";
+    cin >> branch;
+    cout << "Marks: ";
+    cin >> marks;
+    s2.input(name, roll, branch, marks);
+ 
+    cout << "\nDetails of student 1:" << endl;
+    s1.show();
+ 
+    cout << "\nDetails of student 2:" << endl;
+    s2.show();
  
  return 0;
  }
